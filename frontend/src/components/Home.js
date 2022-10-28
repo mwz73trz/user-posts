06b5ee1080e392401d21cc68297cf9dc6951dd22
mwz73trz <michaelzura@bellsouth.net>
2022-10-28 12:17:48 -0400

    added search

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,11 @@
 import "../App.css";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import userPostsAPI from "../api/userPostsAPI";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [posts, setPosts] = useState([]);
 
   const loadPosts = async () => {
@@ -19,13 +20,36 @@ const Home = () => {
   return (
     <div className="container-fluid side-container">
       <div className="row side-row">
-        <ListGroup as="ol" numbered>
-          {posts.map((post) => (
-            <ListGroup.Item variant="dark" key={post.id}>
-              <Link to={`/posts/${post.id}`}>{post.title}</Link>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
+        <nav>
+          <Form.Control
+            style={{ width: "30%" }}
+            placeholder="Search a post"
+            value={searchParams.get("filter") || ""}
+            onChange={(event) => {
+              let filter = event.target.value;
+              if (filter) {
+                setSearchParams({ filter });
+              } else {
+                setSearchParams({});
+              }
+            }}
+          />
+          <p></p>
+          <ListGroup as="ol" numbered>
+            {posts
+              .filter((post) => {
+                let filter = searchParams.get("filter");
+                if (!filter) return true;
+                let searchTerm = post.title.toLowerCase();
+                return searchTerm.startsWith(filter.toLowerCase());
+              })
+              .map((post) => (
+                <ListGroup.Item variant="dark" key={post.id}>
+                  <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                </ListGroup.Item>
+              ))}
+          </ListGroup>
+        </nav>
       </div>
     </div>
   );
